5e57f01f3c5656c8e32dccd86f61ff2ec00a82ea
clz2024.sun <clz2024.sun@gmail.com>
2024-07-29 11:09:47 +0900

    2024-07-29

---
+++ src/com/javaex/book/BookSelectAll.java
@@ -0,0 +1,99 @@
+package com.javaex.book;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookSelectAll {
+
+	public static void main(String[] args) {
+
+		List<Book2Vo> bookList = new ArrayList<Book2Vo>();
+		
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:mysql://localhost:3306/book_db";
+			conn = DriverManager.getConnection(url, "book", "book");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			//*sql문 준비
+			String query = "";
+			query += " select 	b.book_id, ";
+			query += "		    b.title, ";
+			query += "          b.pubs, ";
+			query += "          b.pub_date, ";
+			query += "          a.author_id, ";
+			query += "          a.author_name, ";
+			query += "          a.author_desc ";
+			query += " from book b, author a ";
+			query += " where b.author_id = a.author_id ";
+			
+			//*바인딩
+			pstmt = conn.prepareStatement(query);
+			
+			//*실행
+			rs= pstmt.executeQuery();
+			
+			// 4.결과처리 //List<BookVo>
+			while(rs.next()) {
+				int bookId = rs.getInt("book_id");
+				String title = rs.getString("title");
+				String pubs = rs.getString("pubs");
+				String pubDate = rs.getString("pub_date");
+				int authorId = rs.getInt("author_id");
+				String authorName = rs.getString("author_name");
+				String authorDesc = rs.getString("author_desc");
+				
+				Book2Vo book2Vo = new Book2Vo(bookId, title, pubs, pubDate, authorId, authorName, authorDesc);
+				
+				bookList.add(book2Vo);
+			}
+			
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			// 5. 자원정리
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+
+		}
+		
+		//출력
+		//개발용
+		//System.out.println(bookList.toString());
+		
+		//진짜 출력은 따로 해볼것
+		for(Book2Vo vo : bookList) {
+			System.out.println(vo.getTitle() + ", " + vo.getName());
+		}
+
+	}
+
+}
